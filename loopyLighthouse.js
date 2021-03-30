5f23ef6800a572f36d74fc6ffcfd88993a42e613
Zain Dewsi <zain.dewsi@gmail.com>
2021-03-30 20:21:09 +0000

    Loopy lighthouse for loop

---
+++ loopyLighthouse.js
@@ -0,0 +1,19 @@
+// We will be writing a program that prints the numbers from 100 to 200 to the console, with three exceptions:
+
+// If the number is a multiple of 3, print the string "Loopy" instead of the number.
+// If the number is a multiple of 4, print the string "Lighthouse" instead of the number.
+// If the number is a multiple of both 3 and 4, print the string "LoopyLighthouse" instead of the number.
+
+for (let i = 100; i <= 200; i++) {
+  if (i % 3 === 0) {
+    if (i % 4 === 0) {
+      console.log("LoopyLighthouse")
+    } else {
+      console.log("Loopy")
+    }
+  } else if (i % 4 === 0) {
+    console.log("Lighthouse")
+  } else {
+    console.log(i);
+  }
+}
