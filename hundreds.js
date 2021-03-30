9dc59f342ca9ef4cf41b70280ced21d4b327e4a0
Zain Dewsi <zain.dewsi@gmail.com>
2021-03-30 23:11:13 +0000

    how many hundreds

---
+++ hundreds.js
@@ -0,0 +1,11 @@
+function howManyHundreds(num) {
+  const hundreds = num / 100
+  const remainder = (num % 100) / 100
+  return hundreds - remainder;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
