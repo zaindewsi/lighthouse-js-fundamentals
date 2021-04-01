35d61e55a2eaae201b9112301a35a30aee88206c
Zain Dewsi <zain.dewsi@gmail.com>
2021-04-01 22:12:46 +0000

    concat and merge sort

---
+++ merge.js
@@ -0,0 +1,10 @@
+function merge(array1, array2){
+  for(let i = 0; i < array2.length; i++){
+    array1.push(array2[i]);
+  }
+  return array1.sort();
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
