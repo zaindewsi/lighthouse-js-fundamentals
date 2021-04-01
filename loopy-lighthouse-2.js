e6649bba7d9f43e775ff1dd841f6db3f7a3004e5
Zain Dewsi <zain.dewsi@gmail.com>
2021-04-01 22:05:55 +0000

    refactor loopy lighthouse v2

---
+++ loopy-lighthouse-2.js
@@ -0,0 +1,18 @@
+function loopyLighthouse(range, multiples, words) {
+
+  for (let i = range[0]; i <= range[1]; i++) {
+    if (i % multiples[0] === 0) {
+      if (i % multiples[1] === 0) {
+        console.log(words[0] + words[1])
+      } else {
+        console.log(words[0])
+      }
+    } else if (i % multiples[1] === 0) {
+      console.log(words[1])
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+console.log(loopyLighthouse([15,90], [2,5], ["zain", "dewsi"]))
